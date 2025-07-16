ccd125c2984c448b60a8082fc7d0b9cd36d483ad
dijretad <dijretad@gmail.com>
2025-07-17 01:38:56 +0200

    Integrate Map Times component - add config variable, includes and component registration

--- src/game/client/gameclient.cpp
+++ src/game/client/gameclient.cpp
@@ -129,6 +129,7 @@ void CGameClient::OnConsoleInit()
 					      &m_RaceDemo,
 					      &m_Rainbow,
 					      &m_MapSounds,
+					      &m_MapTimes,
 					      &m_Background, // render instead of m_MapLayersBackground when g_Config.m_ClOverlayEntities == 100
 					      &m_MapLayersBackground, // first to render
 					      &m_BgDraw,
